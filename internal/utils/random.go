71c3b192582e89dcbfd32ba7987306f45f8e9798
Agus Supriyatna <aguzsupriyatna7@gmail.com>
2024-01-21 14:15:59 +0700

    🚀 feat(account_test.go): add unit tests for account CRUD operations to ensure correctness and reliability 🚀 feat(entry_test.go): add unit tests for entry CRUD operations to ensure correctness and reliability 🚀 feat(transfer_test.go): add unit tests for transfer CRUD operations to ensure correctness and reliability 🚀 feat(main_test.go): add test setup and teardown functions to connect to the database and initialize test queries 🚀 feat(random.go): add utility functions to generate random integers, strings, and currencies for testing purposes

---
+++ internal/utils/random.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+var r *rand.Rand
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	r = rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + r.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomCurrency() string {
+	currencies := []string{"IDR", "MYR", "SGD", "USD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
