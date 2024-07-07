5520941f773ddc5b888594394d65af501c0925dd
Agus Supriyatna <agus@motionpay.id>
2024-07-07 13:15:32 +0700

    Load config & env

--- main.go
+++ main.go
@@ -5,18 +5,18 @@ import (
 	"log"
 	"pocikode/simple-bank/api"
 	db "pocikode/simple-bank/db/sqlc"
+	"pocikode/simple-bank/util"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://default:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8012"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -24,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	if err = server.Start(serverAddress); err != nil {
+	if err = server.Start(config.ServerAddress); err != nil {
 		log.Fatal("cannot start server:", err)
 	}
 }
