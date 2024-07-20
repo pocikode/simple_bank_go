778ca9e5df34e3cd7579ecdaebb419f27caf5a66
Agus Supriyatna <agus@motionpay.id>
2024-07-21 01:54:46 +0700

    extract gRPC metadata

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -36,12 +36,14 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		return nil, status.Errorf(codes.Internal, "error creating refresh token")
 	}
 
+	metadata := server.extractMetadata(ctx)
+
 	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
 		ID:           refreshPayload.ID,
 		Username:     user.Username,
 		RefreshToken: refreshToken,
-		UserAgent:    "",
-		ClientIp:     "",
+		UserAgent:    metadata.UserAgent,
+		ClientIp:     metadata.ClientIP,
 		IsBlocked:    false,
 		ExpiresAt:    refreshPayload.ExpiredAt,
 	})
